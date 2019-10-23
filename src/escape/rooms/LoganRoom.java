e1780e2dd2e9f5ca9ea59f8bc79f9f4582b68967
Chris Struck <chrisblammo123@gmail.com>
2019-10-22 20:45:00 -0500

    Started adding custom battleship game. Added ship fitting function

--- src/escape/rooms/LoganRoom.java
+++ src/escape/rooms/LoganRoom.java
@@ -16,12 +16,57 @@ public class LoganRoom extends Room {
 	/**
 	 * My custom exploration room.
 	 */
-	
+	public boolean shipFits(int [][] computerBoard, int x, int y, int Direction, int shipLength) {
+		boolean fits = true;
+		
+		if (Direction == 0) {
+			if (y-shipLength<0) {
+				fits = false;
+			}
+		}
+		if (Direction == 1) {
+			if (x+shipLength>computerBoard.length) {
+				fits = false;
+			}
+		}
+		if (Direction == 2) {
+			if (y+shipLength>computerBoard[1].length) {
+				fits = false;
+			}
+		}
+		if (Direction == 3) {
+			if (x-shipLength<0) {
+				fits = false;
+			}
+		}
+		
+		return fits;
+	}
 	@Override
 	public void exploreRoom() {
+		//battleship
+
+		int [][] computerBoard = {
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0}};
+		int shipLength = 2;
+		int Direction = (int)(Math.random()*4);
+		int x = (int)(Math.random()*computerBoard.length);
+		int y = (int)(Math.random()*computerBoard[1].length);
 		
-		System.out.println("you enter a dark room");
-		System.out.print ("press enter to continue...");
+		if (Direction == 0) {
+			if (y-Shiplength<0) {
+				
+			}
+		}
 		
 		_in.nextLine();
 		
